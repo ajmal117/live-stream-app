7b88ae71b07c9ddf7fa9a28bc1c206ebbfcedc1f
ajmal ali <ajmal.mzn95@gmail.com>
2023-09-27 20:26:13 +0530

    small screen update

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,11 +18,11 @@ export const Home = () => {
       <div
         className="bg-no-repeat bg-cover bg-center relative"
         style={{
-          "background-image":
+          "backgroundImage":
             "url(https://images.unsplash.com/photo-1579621970563-ebec7560ff3e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80)",
         }}
       >
-        <div className="absolute bg-gradient-to-b from-green-500 to-green-400 opacity-30  md:opacity-75 inset-0 z-0"></div>
+        <div className="absolute bg-gradient-to-b from-green-500 to-green-400 opacity-30  md:opacity-75  z-0"></div>
         <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
           <div className="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
             <div className="self-start hidden lg:flex flex-col  text-white">
